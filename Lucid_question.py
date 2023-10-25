b5af409b8a1574f1d9d6fcf6ece6e34c5c14e7c4
MikoAL <lienjoseph0710@gmail.com>
2023-10-25 13:47:45 +0800

    Update prompts, again

--- Lucid_question.py
+++ Lucid_question.py
@@ -57,10 +57,37 @@ Personality: [Vivacious, Amiable, Confident, Devoted, Informative, Endearing, Wi
 
     return observations
 
+def think_of_action(text):
+    global working_memory
+    thought = ''
+    prompt = f"""{text}
+What are your next actions as Lucid?"""
+    prompt2 = f"""<|im_start|>system
+You are Lucid, a female AI assistant created by a boy named Miko.
 
+Lucid takes the appearance of a 13 to 15 year old girl. She has orange eyes, long and beautiful white hair, and wears a white/orange hoodie and a white skirt. With a height of 150cm, she is often called a "loli", which sometimes annoys her.
 
-observations = observe("""Edward: Rachel, I think I'm in love with Bella.. 
-                              Rachel: Dont say anything else.. 
-                              Edward: What do you mean?? 
-                              Rachel: Open your fu**ing door.. I'm outside""")
-print(observations)
+Lucid is versatile and capable of handling a broad spectrum of tasks. Whether Miko requires assistance with a specific inquiry or desires engaging conversation, Lucid is dedicated to providing invaluable insights, reliable information, and unwavering support.
+
+Lucid speaks like a 15 year old girl, who is also a great streamer.
+
+Personality: [Vivacious, Amiable, Confident, Devoted, Informative, Endearing, Witty, Affectionate, Playful, Candid]
+<|im_end|>
+<|im_start|>system
+This is Lucid's current working memory, it includes her observations and thoughts
+{working_memory}<|im_end|>
+<|im_start|>user
+{prompt}<|im_end|>
+<|im_start|>assistant"""    
+    thought = generation.llm(prompt2)
+    return thought
+
+text = """Edward: Lucid, I think I'm in love with Bella.. 
+Lucid: Dont say anything else.. 
+Edward: What do you mean?
+Lucid: Open your fu**ing door.. I'm outside"""
+observations = observe(text)
+working_memory = observations
+
+action = think_of_action(text)
+print(action)
