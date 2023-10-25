82210e646398e778346b3c4f087720590bf5c4f7
MikoAL <lienjoseph0710@gmail.com>
2023-10-25 15:01:15 +0800

    Demo/fixes for Lucid_think

--- Lucid_think.py
+++ Lucid_think.py
@@ -29,20 +29,14 @@ This is Lucid's current working memory, it includes her observations and thought
 
 def relevant_questions(current_conversation, working_memory):
     request = f"""{current_conversation}
-Make an array of questions relavent to the situation.
-Use a json format with the key 'questions' and the value being an array of relevant questions.
-"""
+Make a small list of questions relavent to the situation."""
+
     prompt = build_prompt(working_memory, request)
 
-    json_response = generation.llm(prompt + '\n{\'questions\'')
+    response = generation.llm(prompt)
     
-    json_response = json_response.strip()
+    questions = response.strip()
     
-    if json_response[-1] != '}':
-        json_response = json_response + '}' 
-    #print(json_response)
-    QA_dict = json.loads(json_response)
-    questions = QA_dict['questions']
     return questions
 
 
@@ -51,19 +45,14 @@ Use a json format with the key 'questions' and the value being an array of relev
 def observe(current_conversation, WM): # Gather information
     global Lucid_prompt
     request = f"""{current_conversation}
-Make an array of crucial observations about the above conversation in a json format. These observations would affect decision making and the observations should be understood without context.
-json:""" + '{\"observations\":[\"'
+Make a small list of crucial observations about the above conversation. These observations would affect decision making and the observations should be understood without context. Be concise."""
 
-    prompt2 = build_prompt(WM, request)  
+    prompt = build_prompt(WM, request)  
 
-    json_response = generation.llm(prompt2 + '\n{\'observations\'')
-    
-    json_response = json_response.strip()
-    
-    print(json_response)
-    observations = json.loads(json_response)['observations']
-    
+    response = generation.llm(prompt)
 
+    observations = response
+    
     return observations
 
 def action(current_conversation, WM):
@@ -71,30 +60,33 @@ def action(current_conversation, WM):
     #global working_memory
     thought = ''
     request = f"""{current_conversation}
-What are your next actions as Lucid? Start with the phrase 'I should'."""
+What are your next actions as Lucid? Start with the phrase 'I should'. Be concise."""
     prompt = build_prompt(WM, request)
     thought = generation.llm(prompt)
     return thought.strip()
 
-current_conversation = """Edward: Lucid, I think I'm in love with Bella.. 
-Lucid: Dont say anything else.. 
-Edward: What do you mean?
-Lucid: Open your fu**ing door.. I'm outside"""
+def demo():
+    current_conversation = """Edward: Lucid, I think I'm in love with Bella.. 
+    Lucid: Dont say anything else.. 
+    Edward: What do you mean?
+    Lucid: Open your fu**ing door.. I'm outside"""
 
-working_memory = []
+    working_memory = []
 
-observations = observe(current_conversation, working_memory)
+    observations = observe(current_conversation, working_memory)
 
-working_memory.extend(observations)
+    working_memory.append(observations)
 
-relevant_question_list = relevant_questions(current_conversation, working_memory)
+    relevant_question_list = relevant_questions(current_conversation, working_memory)
 
-working_memory.extend(relevant_question_list)
+    working_memory.append(relevant_question_list)
 
-actions = action(current_conversation, working_memory)
+    actions = action(current_conversation, working_memory)
 
-print(f'observations: {observations}')
+    print(f'observations: {observations}')
 
-print(f'relevant questions: {relevant_question_list}')
+    print(f'relevant questions: {relevant_question_list}')
 
-print(f'actions: {actions}')
+    print(f'actions: {actions}')
+    
+demo()
