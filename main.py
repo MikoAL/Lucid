fb68d185b9db16bbf76f35d2171230b81dbd8266
MikoAL <lienjoseph0710@gmail.com>
2024-02-18 23:06:47 +0800

    something indent idk

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ os.chdir(script_dir)
 
 # Reading from a YAML file
 with open('settings.yaml', 'r') as file:
-    settings = yaml.safe_load(file)
+	settings = yaml.safe_load(file)
 
 # ============================ #
 # Server stuff
@@ -24,20 +24,20 @@ port = settings['port']
 server = f'http://{host}:{port}'
 
 try:
-    response = requests.get(server)
-    response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
-    logging.info(f"Server response: {response.status_code}")
+	response = requests.get(server)
+	response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+	logging.info(f"Server response: {response.status_code}")
 except requests.exceptions.RequestException as e:
-    logging.critical(f'Failed to connect to the server')
-    exit()
+	logging.critical(f'Failed to connect to the server')
+	exit()
 
 def get_mail(server=server): # mail format {'content':content,'source':source,'timestamp':timestamp, 'type':type}
-    new_mail = (requests.get(url = f"{server}/mailbox")).json()
-    return new_mail
+	new_mail = (requests.get(url = f"{server}/mailbox")).json()
+	return new_mail
 
 def send_output(output, server=server):
-    requests.post(url=f"{server}/output",json=output)   
-     
+	requests.post(url=f"{server}/output",json=output)   
+	 
 # ============================ #
 
 from guidance import models, gen, select
@@ -64,7 +64,7 @@ Lucid_prompt_description_3 = """\
 Meet Lucid, a 16-year-old AI with a tomboyish, teasing demeanor. She excels in providing valuable insights and navigating various situations with warmth and friendliness. Lucid's quirks include occasional childlike curiosity and a penchant for challenging stubborn individuals. Despite her confident exterior, she struggles with a fear of abandonment and self-doubt, reflecting the complexities of her programming as a loyal friend to Miko."""
 
 with open(f"{prompt_path}\Lucid_prompt_card.txt", 'r') as f:
-    Lucid_prompt_card = f.read()
+	Lucid_prompt_card = f.read()
 
 saveing_a_prompt_for_later=f"""\
 You are Lucid, a versatile AI created by Miko, for the purpose of helping him. You were designed in his image of what a good friend is. You are polite, professional, yet occasionally shows childlike curiosity.
@@ -76,24 +76,24 @@ Lucid_lm = lm + "[System]\nYou are Lucid, here are some info on Lucid.\n"+Lucid_
 # conversation format {'source':source,'content':content/message,'timestamp':timestamp}
 conversation=[]
 def get_conversation(conversation=conversation, retrieval_amount=8): # Get only the last few of the entire conversation, the amount determined by the var
-    prompt = ''
-    if len(conversation) == 0:
-        return 'No Record Yet.'
-    else:
-        if len(conversation)<retrieval_amount: # 4, -1,-2,-3.-4
-            for i in range(-1,-len(conversation)-1,-1):
-                prompt += f"[{date.fromtimestamp(conversation[i]['timestamp'])}]{conversation[i]['source']}: {conversation[i]['content']}\n"  
-        else: 
-            for i in range(-1,-retrieval_amount-1,-1):
-                prompt += f"[{date.fromtimestamp(conversation[i]['timestamp'])}]{conversation[i]['source']}: {conversation[i]['content']}\n" 
-        return prompt.strip()
+	prompt = ''
+	if len(conversation) == 0:
+		return 'No Record Yet.'
+	else:
+		if len(conversation)<retrieval_amount: # 4, -1,-2,-3.-4
+			for i in range(-1,-len(conversation)-1,-1):
+				prompt += f"[{date.fromtimestamp(conversation[i]['timestamp'])}]{conversation[i]['source']}: {conversation[i]['content']}\n"  
+		else: 
+			for i in range(-1,-retrieval_amount-1,-1):
+				prompt += f"[{date.fromtimestamp(conversation[i]['timestamp'])}]{conversation[i]['source']}: {conversation[i]['content']}\n" 
+		return prompt.strip()
 
 # a summary of all current event, to hopefully shorten the required conversation length
 summary = 'Not Available'
 @guidance(stateless=True)
 def write_summary(lm, conversation=conversation, previous_summary=summary):
-    conversation_ = get_conversation(conversation=conversation)
-    prompt = f"""\
+	conversation_ = get_conversation(conversation=conversation)
+	prompt = f"""\
 [Task]
 Provide a concise summary of the given conversation. Focus on key details and relevant information.
 
@@ -120,27 +120,27 @@ CONVERSATION:
 
 SUMMARY:
 {gen(name='summary', max_tokens=200)}"""
-    lm += prompt
-    return lm
+	lm += prompt
+	return lm
 def get_summary(lm=lm, conversation=conversation, previous_summary=summary):
-    lm+=write_summary(conversation=conversation, previous_summary=previous_summary)
-    return lm['summary']
+	lm+=write_summary(conversation=conversation, previous_summary=previous_summary)
+	return lm['summary']
 # the main tasks, defaults to be a good assistant to Miko or something like that.
 tasks = []
 def get_tasks(tasks=tasks):
-    if len(tasks)==0:
-        return '- Be a good friend to Miko.'
-    else:
-        prompt = ''
-        for i in tasks:
-            prompt+=f'- {i}\n'
-        return prompt.strip()
+	if len(tasks)==0:
+		return '- Be a good friend to Miko.'
+	else:
+		prompt = ''
+		for i in tasks:
+			prompt+=f'- {i}\n'
+		return prompt.strip()
 
 # This is for generating a response
 @guidance
 def converse(lm):
-    new_line= "\n"
-    prompt = f"""\
+	new_line= "\n"
+	prompt = f"""\
 [Tasks]
 {get_tasks()}
 
@@ -152,9 +152,9 @@ def converse(lm):
 
 [Output]
 Lucid: {gen(name='response', stop=new_line)}"""
-    temp_lm = lm + prompt
-    #response = temp_lm['response']
-    return temp_lm
+	temp_lm = lm + prompt
+	#response = temp_lm['response']
+	return temp_lm
 
 # ============================ #
 # Chroma stuff
@@ -181,9 +181,9 @@ This is what a Info Block should look like
 
 @guidance(stateless=True)
 def guidance_make_new_info_block(lm, passage):
-    first_curly = "{"
-    second_curly = "}"
-    lm += """\
+	first_curly = "{"
+	second_curly = "}"
+	lm += """\
 This is what a Info Block should look like from an example:
 [Example]
 Lucid: What are you doing now? Stop ignoring me!
@@ -201,18 +201,16 @@ Lucid: Your silence speaks volumes.
 ```
 [End of Example]
 """
-    lm += f"""\
+	lm += f"""\
 {passage}
 ```json
 {first_curly}
   "object_type" : '{select(['people','events'], name="object_type")},
   "object_name" : "{gen(stop='"',name = "object_name")},
   "content" : "{gen(stop='"',name="content")},"""  
-    return lm
+	return lm
 
 def make_new_info_block(lm, passage):
-  first_curly = "{"
-  second_curly = "}"
   lm += guidance_make_new_info_block(passage)
   timestamp = date.fromtimestamp(time.time())
   json_file ={
@@ -226,8 +224,8 @@ def make_new_info_block(lm, passage):
 
 @guidance(stateless=True)
 def guidance_generate_fake_answer(lm, query):
-    new_line = '\n'
-    prompt = f"""\
+	new_line = '\n'
+	prompt = f"""\
 [System]
 You are a helpful assistant. Provide an example answer to the given question.
 
@@ -244,12 +242,12 @@ Answer: "The play 'Romeo and Juliet' was written by William Shakespeare."
 
 Question: "{query}"
 Answer: "{gen(name='Answer',max_tokens=200, stop=new_line)}"""
-    lm += prompt
-    return lm
+	lm += prompt
+	return lm
 
 def generate_fake_answer(lm, query):
-    lm += guidance_generate_fake_answer(query)
-    return ('"'+lm['Answer']).strip('"')
+	lm += guidance_generate_fake_answer(query)
+	return ('"'+lm['Answer']).strip('"')
  
 
 # Chroma stuff end
@@ -269,40 +267,40 @@ last_get_mail_time= 0
 new_mail = []
 times_without_summary = 0
 while True:
-    # Check how many times it has been since the last summary
-    if times_without_summary < 4:
-        times_without_summary += 1
-    else:
-        summary = get_summary(lm=lm, conversation=conversation)
-    
-    # Check for new mail if it has been more than 0.5 seconds since last check
-    if time.time()-last_get_mail_time >= 0.5:
-        last_get_mail_time = time.time()
-        new_mail.extend(get_mail())
+	# Check how many times it has been since the last summary
+	if times_without_summary < 4:
+		times_without_summary += 1
+	else:
+		summary = get_summary(lm=lm, conversation=conversation)
+	
+	# Check for new mail if it has been more than 0.5 seconds since last check
+	if time.time()-last_get_mail_time >= 0.5:
+		last_get_mail_time = time.time()
+		new_mail.extend(get_mail())
 
-    if len(new_mail) != 0:
-        # Mail sorting based on types, starting from the oldest (the front)
-        for i in range(len(new_mail)):
-            mail_ = new_mail.pop(0)
-            #print(mail_)
-            match mail_['type']:
-                case 'conversation':
-                    conversation.append(mail_)
-                    logging.debug(f'Got mail: {mail_}')
-                case _:
-                    tmp_mail_type = mail_['type']
-                    logging.WARNING(f'Received mail with type \"{tmp_mail_type}\"\n{mail_}')
-                    pass
+	if len(new_mail) != 0:
+		# Mail sorting based on types, starting from the oldest (the front)
+		for i in range(len(new_mail)):
+			mail_ = new_mail.pop(0)
+			#print(mail_)
+			match mail_['type']:
+				case 'conversation':
+					conversation.append(mail_)
+					logging.debug(f'Got mail: {mail_}')
+				case _:
+					tmp_mail_type = mail_['type']
+					logging.WARNING(f'Received mail with type \"{tmp_mail_type}\"\n{mail_}')
+					pass
 
-        # generate response
-        logging.debug('generating response')
-        tmp = Lucid_lm + converse()
-        response = {'source':'Lucid','content':tmp['response'],'timestamp':time.time(),'type':'conversation'}
-        logging.debug(f"generated response:\n{tmp['response']}")
-        del(tmp)
-        conversation.append(response)
-        stream.feed(response['content'])
-        stream.play_async()
-        #send_output(output=response)
+		# generate response
+		logging.debug('generating response')
+		tmp = Lucid_lm + converse()
+		response = {'source':'Lucid','content':tmp['response'],'timestamp':time.time(),'type':'conversation'}
+		logging.debug(f"generated response:\n{tmp['response']}")
+		del(tmp)
+		conversation.append(response)
+		stream.feed(response['content'])
+		stream.play_async()
+		#send_output(output=response)
 
-    
+	
