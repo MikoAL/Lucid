50243b374c87ba9b5db5da14a7b76cfb54e58037
MikoAL <lienjoseph0710@gmail.com>
2024-03-03 13:03:18 +0800

    Add server status to dashboard

--- display.py
+++ display.py
@@ -1,34 +1,55 @@
+import os
+import time
+import requests
+import yaml
+
 from textual.app import App, ComposeResult, RenderResult
 from textual.containers import Container, Horizontal, Vertical, ScrollableContainer, VerticalScroll
 from textual.widgets import Button, Footer, Header, Static, Placeholder, Welcome, Label, Input, Markdown, RichLog
 from textual.widget import Widget
+from textual.message import Message
 from textual import events
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+# Change the working directory to the script's directory
+os.chdir(script_dir)
 
+# Reading from a YAML file
+with open('settings.yaml', 'r') as file:
+	settings = yaml.safe_load(file)
+
+host = settings['host']
+port = settings['port']
+server = f'http://{host}:{port}'
+
+user_name = settings['user_name']
 
 class MainWindows(Vertical):
-    def __init__(self):
-        super().__init__()
-        self.compose()
-        self.chatlog = ChatLog()
-        self.chatinput = ChatInput()
+	def __init__(self):
+		super().__init__()
+		self.compose()
+		self.styles.width = "2fr"
+		self.styles.height = "1fr"
+		self.chatlog = ChatLog()
+		self.chatinput = ChatInput()
 
-    def on_mount(self) -> None:
-        self.styles.outline = ("round", "white")
-        self.styles.width = "2fr"
+	#def on_mount(self) -> None:
+		#self.styles.outline = ("round", "white")
 
-    def compose(self) -> ComposeResult:
 
-        yield self.chatlog
-        yield self.chatinput
+	def compose(self) -> ComposeResult:
+
+		yield self.chatlog
+		yield self.chatinput
 
 
 class ChatLog(RichLog):
 	def __init__(self):
 		super().__init__()
 		self.compose()
-
-		self.styles.outline = ("round", "white")
+	def on_mount(self):
+		self.styles.margin = (1, 1)
+		self.styles.outline = ("ascii", "white")
 		self.styles.width = "1fr"
 		self.styles.height = "2fr"
 		self.styles.align = ("center", "middle")
@@ -42,14 +63,16 @@ class ChatInput(Container):
 	def __init__(self):
 		super().__init__()
 		self.input_box = Input()
-		self.compose()
-	def on_mount(self) -> None:
+	def on_mount(self):
+		self.styles.margin = (1, 1)
 		self.styles.height = "1fr"
 		self.styles.width = "1fr"
 		self.styles.outline = ("round", "white")
 		self.input_box.styles.align = ("center", "middle")
 		self.input_box.styles.width = "1fr"
 		self.input_box.styles.height = "1fr"
+
+
 	
 	def on_input_submitted(self, message: Input.Submitted) -> None:
 		self.input_box.clear()
@@ -59,17 +82,53 @@ class ChatInput(Container):
 
 
 class DashBoard(Vertical):
-    	
+		
 	def on_mount(self) -> None:
 		self.styles.width = "1fr"
 		self.styles.height = "1fr"
+		#self.styles.outline = ("round", "white")
+	def compose(self) -> ComposeResult:
+		yield ServerStatus()
+		yield Placeholder()
+		yield Placeholder()
+	#def render(self) -> RenderResult:
+	#	return ServerStatus()
+class ServerStatus(Label):
+	def __init__(self):
+		super().__init__()
+		self.styles.width = "1fr"
+		self.styles.height = "1fr"
+		self.styles.margin = (1, 1)
 		self.styles.outline = ("round", "white")
+		self.renderable = "Server is not alive"
+
   
-	def render(self) -> RenderResult:
-		return "DashBoard"
+	#def on_mount(self) -> None:
+	#	self.styles.width = "1fr"
+	#	self.styles.height = "1fr"
+	#	self.styles.outline = ("round", "white")
+  
+	class ServerAlive(Message):
+		def __init__(self, server_alive: bool):
+			self.server_alive = server_alive
+			super().__init__()
+
+	def server_alive(self) -> None:
+		try:
+			response = requests.get(server)
+			self.post_message(self.ServerAlive(server_alive=True))
+		except requests.exceptions.RequestException as e:
+			self.post_message(self.ServerAlive(server_alive=False))
+   
+	def on_server_alive(self, message: ServerAlive) -> None:
+		if message.server_alive:
+			self.renderable = "Server is alive"
+		else:
+			self.renderable = "Server is not alive"
+
 
 class Root(App):
-	#CSS_PATH = "textual_test.tcss"
+	CSS_PATH = "display.tcss"
 	def __init__(self):
 		super().__init__()
 		self.dash_board = DashBoard()
